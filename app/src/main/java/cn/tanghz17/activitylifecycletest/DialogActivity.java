ab3e13da43f4099ccd3945eddfd41e6d1d73d13c
TANGhz17 <42084546+TANGhz17@users.noreply.github.com>
2020-12-22 18:49:41 +0800

    Initial commit

---
+++ app/src/main/java/cn/tanghz17/activitylifecycletest/DialogActivity.java
@@ -0,0 +1,14 @@
+package cn.tanghz17.activitylifecycletest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DialogActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.dialog_layout);
+    }
+}
